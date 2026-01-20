5e2fb388fc85cc7b85ef5cb40076408e56ea084c
imharshgautam <meharshgautam@gmail.com>
2026-01-20 22:06:58 +0530

    Add backend server setup

---
+++ server/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+// function on connect to the database
+
+export const connectDB = async () => {
+    try {
+
+        mongoose.connection.on("connected", ()=> console.log('DataBase Connected'));
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
+    } catch (error) {
+        console.log(error);
+    }
+}
