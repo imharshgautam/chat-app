0a04932887dc5bf8b75ff408e0135a20d0ea7c75
imharshgautam <meharshgautam@gmail.com>
2026-01-28 18:08:56 +0530

    fix chnages for deployment

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ const server = http.createServer(app);
 
 // Initilize socket.io server
 
-export const io = new Server(server,{
-    cors : {origin: "*"}
+export const io = new Server(server, {
+    cors: { origin: "*" }
 })
 
 // store online users
@@ -25,32 +25,32 @@ export const userSocketMap = {};        // {userId: socketId}
 
 // socket.io connection handler
 
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User Connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
+    if (userId) userSocketMap[userId] = socket.id;
 
     // emit online users to all connected clients
 
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnected",userId);
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap))
-        
+
     })
-    
+
 })
 
 // middleware setup
 
-app.use(express.json({limit : "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 // routes setup
-app.use("/api/status", (req, res)=>( res.send("server is live")));
+app.use("/api/status", (req, res) => (res.send("server is live")));
 app.use("/api/auth", userRouter)
 app.use("/api/messages", messageRouter)
 
@@ -58,11 +58,11 @@ app.use("/api/messages", messageRouter)
 
 await connectDB();
 
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 8000;
-    server.listen(PORT, ()=> console.log("Server is running on PORT : "+ PORT));
+    server.listen(PORT, () => console.log("Server is running on PORT : " + PORT));
 }
 
 // export server to vercel 
 
-export default server;
+export default app;
